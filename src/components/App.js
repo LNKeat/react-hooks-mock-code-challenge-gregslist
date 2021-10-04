700a284ed8e39512f676b500629a4a0922ec1e96
Laura <lnkeat@gmail.com>
2021-10-03 22:05:31 -0600

    commit tree f7b154d5e42cd449e0f8fc96a646957fd63d38bd

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:6001/listings/')
+    .then(res => res.json())
+    .then(listings => setListings(listings))
+  }, [])
+
+  function handleListingDelete(listing){
+    console.log(listing)
+    fetch(`http://localhost:6001/listings/${listing.id}`, {
+      method: 'DELETE', 
+    })
+    .then(res => res.json())
+    .then(() => {
+      const filteredListings = listings.filter(oldListing => oldListing.id !== listing.id)
+      setListings(filteredListings)
+    })
+  }
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings} handleListingDelete={handleListingDelete} />
     </div>
   );
 }
